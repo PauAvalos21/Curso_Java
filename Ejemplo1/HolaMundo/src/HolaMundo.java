ae1b829f94df5af3775c86208e3612a306ba948e
Paulina Monserrath Avalos Cervantes <pauavalos1502@gmail.com>
2025-04-08 16:12:53 -0600

    Sesión 1 Ejemplo 1

---
+++ Ejemplo1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,5 @@
+public class HolaMundo { //Definir clase publica que debe llamarse igual al archivo
+    public static void main(String[] args) { //main es el responsable de ejecutar el codigo "clase o metodo principal"
+        System.out.println("¡Hola, Java!"); //imprime texto en pantalla
+    }
+}
